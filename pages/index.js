5354c929c2ff08a4698a90d71f8d5259acaf4d91
Pavel Herzik <pavel@herzik.ru>
2022-11-30 10:48:09 +0300

    Fix js logic

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,21 @@ const colorCodeElement = document.querySelector('.color-code'),
   colorChangerButton = document.querySelector('.color-changer'),
   container = document.querySelector('.container')
 
+function getRandomNumber() {
+  return Math.ceil(Math.random() * 255)
+}
+
+function generateRgb() {
+  return `rgb(${getRandomNumber()}, ${getRandomNumber()}, ${getRandomNumber()})`
+}
+
 function changeColor() {
-  const backgroundColor = `rgb(${Math.ceil(Math.random(9) * 100)}, ${Math.ceil(
-    Math.random(9) * 100
-  )}, ${Math.ceil(Math.random(9) * 100)})`
+  const backgroundColor = generateRgb()
   colorCodeElement.style.color = backgroundColor
   container.style.backgroundColor = backgroundColor
   colorCodeElement.textContent = backgroundColor
 }
 
 colorChangerButton.addEventListener('click', changeColor)
+
+console.log(Math.random(2) * 10)
